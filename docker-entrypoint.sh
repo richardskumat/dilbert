0b6ab34ea7e16636b38174860589ec0c40c20517
Richard Skumat <gitlab@richardskumat.com>
2019-10-10 20:02:01 +0100

    remove 2 useless steps from docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,5 @@ PUID=${PUID:-1000}
 GUID=${GUID:-1000}
 usermod -u ${PUID} user
 groupmod -g ${GUID} user
-cd /home/user
-chmod 0700 dilbert date-generator.py
 chown user:user -R /home/user
 su user -c "/home/user/dilbert ${@}"
